d288f5920d0e404c48ca32be3e95ff55aba7f108
hmurph3.uw.edu <hannah@DESKTOP-F5OL938.localdomain>
2019-04-30 17:14:01 -0700

    making changes to reflect TA feedback

--- using_functions.py
+++ using_functions.py
@@ -9,8 +9,23 @@ dataframe matches the City of Seattle Wage Data:
 import pandas as pd
 
 # import City of Seattle Wage Data
-wages = pd.read_csv('https://data.seattle.gov/api/views/2khk-5ukd/rows.csv?'
-                    'accessType=DOWNLOAD', sep=',', header=0)
+def create_data_frame():
+    """
+    Creates a dataframe from the City of Seattle Wage Data
+    Returns the dataframe if there are more than three columns otherwise
+    it returns "Dataframe as less than three columns"
+    """
+    wages = pd.read_csv('https://data.seattle.gov/api/views/2khk-5ukd/'
+                        'rows.csv?accessType=DOWNLOAD', sep=',', header=0,
+                        names=("Department", "LastName", "FirstName",
+                               "JobTitle", "HourlyRate"),
+                        dtype={"Department": str, "LastName": str,
+                               "FirstName": str, "JobTitle": str,
+                               "HourlyRate": float})
+    if wages.shape[1] > 3:
+        return wages
+    else:
+        raise ValueError("Unexpected column titles")
 
 
 def test_create_dataframe(df):
@@ -21,9 +36,13 @@ def test_create_dataframe(df):
     - the columns contain the correct data type
     - there are at least 10 rowns in df
     """
-    if (df.shape[0] >= 10 and
-        list(wages) == list(df) and
-            wages.dtypes.equals(df.dtypes)):
+    column_titles = ["Department", "LastName", "FirstName", "JobTitle",
+                     "HourlyRate"]
+
+    if (all([title in df.columns for title in column_titles]) and
+            list(column_titles.columns) == list(df.columns) and
+            df.shape[0] > 10 and data.dtypes.Department == str and
+            df.dtypes.LastName == str and df.dtypes.FirstName == str and
+            df.dtypes.JobTitle == str and df.dtypes.HourlyRate == float):
         return True
-    else:
-        return False
+
